c98917fb6bd6cd8fe11cbd195888a4e330ea3fdc
Gustavo Santana <gustavofelipe68@gmail.com>
2019-09-14 22:13:47 -0300

    Heroku

--- src/config/database.js
+++ src/config/database.js
@@ -2,16 +2,9 @@ const Sequelize = require('sequelize');
 
 
 module.exports = new Sequelize({
-    username: "zhfelgmy",
-    password: "83CX2eKkSG2W2HaN3HFsI-LFbwW2MOyJ",
-    database: "zhfelgmy",
-    dialect: "postgres",
-    host: "motty.db.elephantsql.com",
-    dialectOptions: {
-      useUTC: false,
-      dateStrings: true,
-      typeCast: true,
-      timezone: "-03:00"
-    },
-    timezone: "-03:00"
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    dialect: process.env.DB_DIALECT,
+    host: process.env.DB_HOST,
 });
