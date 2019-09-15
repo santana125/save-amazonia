9aa2ef23dc0ead1c65947a0b3acf89ae7ab26985
Gustavo Santana <gustavofelipe68@gmail.com>
2019-09-14 23:00:26 -0300

    Heroku

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 5000;
 const postRoutes = require('./routes/posts');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
+console.log(process.env);
 
 const db = require('./config/database');
 
