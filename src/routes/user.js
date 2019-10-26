51d061a0cc04c97608786624b38d14375d59793a
Gustavo Santana <gustavofelipe68@gmail.com>
2019-10-26 19:17:51 -0300

    Added File Upload

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,9 +2,31 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const multer = require('multer');
+const path = require('path');
 
 
-router.post('/user', userController.store);
+const multerConfig = {
+    dest: path.resolve(__dirname, '..', '..', 'public', 'profile_pic' );
+    storage: multer.diskStorage({
+
+    }),
+    limits: {
+      size: 4 * 1024 * 1024
+    },
+    fileFilter: (req, file, cb) => {
+      allowedMimes: {
+        'image/png',
+        'image/jpg',
+        'image/jpeg',
+      }
+      allowedMimes.includes(file.mimeType) ?
+        cb(null, true)
+      :
+        cb(new Error('Invalid File'))
+    },
+
+}
+router.post('/user',multer().single('profile_pic'), userController.store);
 
 module.exports = router;
-
